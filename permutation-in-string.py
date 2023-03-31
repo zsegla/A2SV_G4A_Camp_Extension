dd461a7fa51931b315e6f958555ed33e067410a9
Fikreselassie <101448903+zsegla@users.noreply.github.com>
2023-03-31 21:37:48 +0300

    Create permutation-in-string.py

---
+++ permutation-in-string.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/permutation-in-string/
+
+
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        N = len(s1)
+        freq = [0]*26
+
+        for c in s1:
+            freq[ord(c) - ord("a")] += 1
+
+        for i, c in enumerate(s2):
+            freq[ord(c)-ord("a")] -= 1
+            if i >= N:
+                freq[ord(s2[i-N]) - ord("a")] += 1  
+            if not any(freq):
+                return True 
+        return False 
+        
